e860d88d5555164bc4446be7c937697f3b164f5b
Robert Macháček <lighthunt.net@gmail.com>
2015-09-08 23:49:14 +0700

    update comments

--- convert-all-subs-here.bat
+++ convert-all-subs-here.bat
@@ -1,5 +1,5 @@
 @echo off
-rem *** covert all in this folder to UTF-8 with BOM
+rem *** covert all *.srt files in this folder from encoding CP1250 to UTF-8 with BOM
 
 mkdir utf8
 FOR %%i IN (*.srt) DO (copy BOM.bin utf8\%%i 
