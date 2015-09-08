398f2d76b1589eb1b9ae6e6e556ae0df39d5e594
Robert Macháček <lighthunt.net@gmail.com>
2015-09-08 23:52:58 +0700

    adding 'src' dir formating

--- convert-all-subs-in-src.bat
+++ convert-all-subs-in-src.bat
@@ -2,5 +2,7 @@
 rem *** covert all *.srt files in this folder from encoding CP1250 to UTF-8 with BOM
 
 mkdir utf8
-FOR %%i IN (*.srt) DO (copy BOM.bin utf8\%%i 
- iconv.exe -f CP1250 -t UTF-8 "%%i" >> "utf8\%%i")
+FOR %%i IN (src\*.srt) DO (
+	copy BOM.bin utf8\%%i 
+	iconv.exe -f CP1250 -t UTF-8 "%%i" >> "utf8\%%i"
+)
