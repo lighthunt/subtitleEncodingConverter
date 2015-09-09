7d555a5180a4e4bc0a850f142fbc86ed3f33a43a
Robert Macháček <lighthunt.net@gmail.com>
2015-09-09 19:07:30 +0700

    fixing src folder issue to make it work again

--- convert-all-subs-in-src.bat
+++ convert-all-subs-in-src.bat
@@ -2,7 +2,9 @@
 rem *** covert all *.srt files in './src' folder from encoding CP1250 to UTF-8 with BOM
 
 mkdir utf8
-FOR %%i IN (src\*.srt) DO (
-	copy BOM.bin utf8\%%i 
-	iconv.exe -f CP1250 -t UTF-8 "%%i" >> "utf8\%%i"
+cd src
+FOR %%i IN (*.srt) DO (
+	copy BOM.bin "..\utf8\%%i" 
+	iconv.exe -f CP1250 -t UTF-8 "%%i" >> "..\utf8\%%i"
 )
+cd ..
